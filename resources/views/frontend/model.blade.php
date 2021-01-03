af2facff5f884242d2bad79a1be617dbcb2f956d
Mina Nabil <mina9492@hotmail.com>
2021-01-03 13:02:55 +0200

    Working on Models

--- resources/views/frontend/model.blade.php
+++ resources/views/frontend/model.blade.php
@@ -103,9 +103,9 @@
             <h5><a href="#">Car Name Text</a></h5>
             <p class="list-price">$90,000</p>
             <ul>
-              <li><i class="fas fa-database" aria-hidden="true"></i>{{$car->CAR_ENCC}} cc</li>
+              <li><i class="fa fa-database" aria-hidden="true"></i>{{$car->CAR_ENCC}} cc</li>
               <li><i class="fa fa-calendar" aria-hidden="true"></i>{{$car->MODL_YEAR}} model</li>
-              <li><i class="fas fa-horse" aria-hidden="true"></i>{{$car->CAR_HPWR}} hp</li>
+              <li><i class="fa fa-horse" aria-hidden="true"></i>{{$car->CAR_HPWR}} hp</li>
               <li><i class="fa fa-tachometer" aria-hidden="true"></i>{{$car->CAR_ACC}}sec to 0-100km/hr</li>
               <li><i class="fa fa-superpowers" aria-hidden="true"></i>{{$car->CAR_TORQ}} kW</li>
               <li><i class="fa fa-car" aria-hidden="true"></i>{{$car->CAR_DIMN}}</li>
