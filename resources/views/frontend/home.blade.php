0a6e3f137af44c08e0bffa33267c676256c3261f
Mina Nabil <mina9492@hotmail.com>
2021-01-12 16:36:58 +0200

    adjusting car image

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -126,7 +126,7 @@
           @foreach ($type->cars as $car)
           <div class="col-list-3">
             <div class="recent-car-list">
-              <div class="car-info-box"> <a href="{{url('car/' . $car->id)}}"><img style="height: 200px; object-fit: fill;" src="{{(isset($car->image)) ? asset( 'storage/'. $car->image ) : 'assets/images/600x380.jpg'}}" class="img-fluid"
+              <div class="car-info-box"> <a href="{{url('car/' . $car->id)}}"><img style="height: 224px; " src="{{(isset($car->image)) ? asset( 'storage/'. $car->image ) : 'assets/images/600x380.jpg'}}" class="img-fluid"
                     alt="image"></a>
                 <div class="compare_item">
                   <div class="checkbox">
