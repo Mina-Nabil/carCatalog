921ccd4d42e4d4f04380b1f0e3ec287a7e31f421
Mina Nabil <mina9492@hotmail.com>
2021-01-20 22:15:59 +0200

    Fixing search names

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -245,6 +245,6 @@ class SiteController extends Controller
             $query = $query->where("CAR_PRCE", "<=", $priceTo);
         }
 
-        return $query->get();
+        return [$query->toSql(), $query->getBindings()];
     }
 }
