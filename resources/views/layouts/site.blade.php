991718d6457c174162248bf5f41101093e13dbd8
Mina Nabil <mina9492@hotmail.com>
2021-01-27 18:25:57 +0200

    Adding Loan calculator

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -272,7 +272,7 @@
 
     <!-- Scripts -->
     <script src="{{asset('assets/frontend/js/jquery.min.js')}}"></script>
-    <script src="{{asset('assets/frontend/js/popper.min.js')}}"></script>
+    <script src="{{ asset('assets/node_modules/popper/popper.min.js') }}"></script>
     <script src="{{asset('assets/frontend/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('assets/frontend/js/interface.js')}}"></script>
     <!--bootstrap-slider-JS-->
