78d5092683db25f9660c6724239bbdaa91b19291
Mina Nabil <mina9492@hotmail.com>
2021-01-29 13:04:21 +0200

    Adding car calculator fix

--- resources/views/frontend/calculator.blade.php
+++ resources/views/frontend/calculator.blade.php
@@ -132,7 +132,7 @@
             <div class="col-md-7">
                 <div class="intro-img-wp">
                     <img @if(isset($frontendData['Calculator Page']['Calculator Car Image']) && strlen($frontendData['Calculator Page']['Calculator Car Image'])>0)
-                    src="{{asset('storage.' . $frontendData['Calculator Page']['Calculator Car Image'])}}"
+                    src="{{asset('storage/' . $frontendData['Calculator Page']['Calculator Car Image'])}}"
                     @else
                     src="{{asset('assets/frontend/images/1243x532.png')}}"
                     @endif
