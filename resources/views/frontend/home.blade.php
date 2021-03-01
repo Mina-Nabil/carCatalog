ca4a95691da25011ba225aea812c6f417f1edb6c
Mina Nabil <mina9492@hotmail.com>
2021-03-01 13:38:33 +0200

    Slides fix

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -19,16 +19,7 @@
               </select>
             </div>
           </div>
-          <div class="form-group col-md-3 col-sm-6">
-            <div class="select">
-              <select class="form-control" name=brandID>
-                <option value=0>Select Brand</option>
-                @foreach($brands as $brand)
-                <option value="{{$brand->id}}">{{$brand->BRND_NAME}}</option>
-                @endforeach
-              </select>
-            </div>
-          </div>
+      
           <div class="form-group col-md-3 col-sm-6">
             <div class="select">
               <select class="form-control" name=modelID>
@@ -50,6 +41,10 @@
             </div>
           </div>
 
+          <div class="form-group col-md-3 col-sm-6">
+            <button type="submit" class="btn btn-block"><i class="fa fa-search" aria-hidden="true"></i> Search Car </button>
+          </div>
+
           <div class="form-group col-md-9 col-sm-6">
             <label class="form-label">Price Range ($) </label>
             <input id="price_range" type="text" class="span2" name="priceRange" data-slider-min="{{$carsMin}}" data-slider-max="{{$carsMax}}" data-slider-step="5"
@@ -57,9 +52,7 @@
           </div>
 
 
-          <div class="form-group col-md-3 col-sm-6">
-            <button type="submit" class="btn btn-block"><i class="fa fa-search" aria-hidden="true"></i> Search Car </button>
-          </div>
+
         </div>
       </form>
 
