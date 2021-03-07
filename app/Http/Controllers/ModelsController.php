0b3c392212acadb4fc0e89d53a3312ada6fd5fa1
Mina Nabil <mina9492@hotmail.com>
2021-03-07 21:21:16 +0200

    site touches

--- app/Http/Controllers/ModelsController.php
+++ app/Http/Controllers/ModelsController.php
@@ -24,7 +24,7 @@ class ModelsController extends Controller
         $this->data['formURL'] = url("admin/models/update");
         $this->data['imageFormURL'] = url("admin/models/add/image");
         $this->data['updateImageInfoURL'] = url("admin/models/update/image");
-        $this->data['delImageURL'] = url("admin/models/image/delete/");
+        $this->data['delImageUrl'] = url("admin/models/image/delete/");
         $this->data['isCancel'] = false;
         return view('models.profile', $this->data);
     }
