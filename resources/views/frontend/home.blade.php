c9191e69bca6b25b87ae973c34712e0adb582b6a
Mina Nabil <mina9492@hotmail.com>
2021-05-23 08:26:10 +0200

    Adding Dark Overlay

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -153,7 +153,7 @@
 @endif
 
 @if(isset($frontendData['Showroom stats']) && $frontendData['Showroom stats']['Active'])
-<section class="fun-facts-section" style='opacity: 0.4; background-image: {{ (isset($frontendData['Showroom stats']['Background Image'])) ? 
+<section class="fun-facts-section" style='background-image: {{ (isset($frontendData['Showroom stats']['Background Image'])) ? 
  'url(' . asset('storage/' . $frontendData['Showroom stats']['Background Image']) . ')' :
  'url(' . asset('assets/frontend/images/1920x400.jpg') . ')' }} !important'>
   <div class="container div_zindex ">
